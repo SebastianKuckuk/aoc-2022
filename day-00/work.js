90f7a4ee3b9fa84c7e42f2a5ea57181881bcd6b9
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2022-12-05 10:26:03 +0100

    add template

---
+++ day-00/work.js
@@ -0,0 +1,14 @@
+function parseInput(input) {
+    return input.split('\n').map(row => row)
+}
+
+module.exports = {
+    part01: function part01(input) {
+        console.log(parseInput(input))
+        return ''
+    },
+
+    part02: function part02(input) {
+        return ''
+    }
+}
